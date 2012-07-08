0f4f4382a57742cd21ad445cb5b156d6ec59bde9
Leif Poorman <leif.poorman@gmail.com>
2012-07-08 00:51:52 -0400

    warn on reflection

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url ""
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :warn-on-reflection true)
