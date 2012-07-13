cc18af7ed7b7cd7a78dee881408ce8200cbaabd3
Leif Poorman <leif.poorman@gmail.com>
2012-07-13 05:16:50 -0400

    Benchmarks are not tests.

--- test/benchmark/dispatch/is_a_protocol.clj
+++ test/benchmark/dispatch/is_a_protocol.clj
@@ -1,6 +1,5 @@
 (ns benchmark.dispatch.is-a-protocol
-  (:use [clojure.test :only (are deftest is testing)]
-        dispatch.is-a-protocol))
+  (:use dispatch.is-a-protocol))
 
 ;; ensure that is-a? is extended to maps
 (defn- map-is-a? [child parent h]
@@ -19,34 +18,34 @@
   (-is-a? [c p h] (map-is-a? c p h)))
 
 (defmacro timeit [msg n & body] 
-  `(println ~msg (with-out-str (time (dotimes [n# ~n] ~@body)))))
+  `(print ~msg (with-out-str (time (dotimes [n# ~n] ~@body)))))
 
 ;;TODO  use local hierarchy?
 (derive ::child ::parent)
 (derive* ::child ::parent)
 
-;;TODO  Should benchmarks really be tests?
-(deftest benchmark-is-a
-  (is 
-    (do
-      (timeit "isa? zero" 10000000 (isa? 0 0))
-      (timeit "is-a? zero" 10000000 (is-a? 0 0))
-      (timeit "isa? nil" 10000000 (isa? nil nil))
-      (timeit "is-a? nil" 10000000 (is-a? nil nil))
-      (timeit "isa? class" 5000000 (isa? Float Number))
-      (timeit "is-a? class" 5000000 (is-a? Float Number))
-      ;;TODO is-a? is 2-3 times slower than isa? on kw, I don't know why
-      ;;it's not true when timing at the REPL.  ???
-      (timeit "isa? keyword" 1000000 (isa? ::child ::parent))
-      (timeit "is-a? keyword" 1000000 (is-a? ::child ::parent))
-      (timeit "isa? deep class" 5000000 
-              (isa? clojure.lang.PersistentHashMap Object))
-      (timeit "is-a? deep class" 5000000 
-              (is-a? clojure.lang.PersistentHashMap Object))
-      (timeit "isa? vector" 500000 (isa? [1 2 3 Float] [1 2 3 Number]))
-      (timeit "is-a? vector" 500000 (is-a? [1 2 3 Float] [1 2 3 Number]))
-      (timeit "isa? vector standing in for a map" 100000 
-              (isa? [:a :b Float] [:a :b Number]))
-      (timeit "isa? map" 100000 
-              (is-a? {0 :a 1 :b 2 Float} {0 :a 1 :b 2 Number}))
-      true)))
+(defn benchmark-is-a []
+  (do
+    (timeit "isa? zero" 10000000 (isa? 0 0))
+    (timeit "is-a? zero" 10000000 (is-a? 0 0))
+    (timeit "isa? nil" 10000000 (isa? nil nil))
+    (timeit "is-a? nil" 10000000 (is-a? nil nil))
+    (timeit "isa? class" 5000000 (isa? Float Number))
+    (timeit "is-a? class" 5000000 (is-a? Float Number))
+    ;;TODO is-a? is 2-3 times slower than isa? on kw, I don't know why
+    ;;it's not true when timing at the REPL.  ???
+    (timeit "isa? keyword" 1000000 (isa? ::child ::parent))
+    (timeit "is-a? keyword" 1000000 (is-a? ::child ::parent))
+    (timeit "isa? deep class" 5000000 
+            (isa? clojure.lang.PersistentHashMap Object))
+    (timeit "is-a? deep class" 5000000 
+            (is-a? clojure.lang.PersistentHashMap Object))
+    (timeit "isa? vector" 500000 (isa? [1 2 3 Float] [1 2 3 Number]))
+    (timeit "is-a? vector" 500000 (is-a? [1 2 3 Float] [1 2 3 Number]))
+    (timeit "isa? vector standing in for a map" 100000 
+            (isa? [:a :b Float] [:a :b Number]))
+    (timeit "isa? map" 100000 
+            (is-a? {0 :a 1 :b 2 Float} {0 :a 1 :b 2 Number}))))
+
+(defn -main "Run benchmarks for is-a?" [& args]
+  (benchmark-is-a))
