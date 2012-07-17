fa95a5672c093a87d0d3babc3687597dd19f622e
Leif Poorman <leif.poorman@gmail.com>
2012-07-17 17:32:57 -0400

    added tests for dispatching Is-A on parent

--- test/dispatch/test/extending_is_a.clj
+++ test/dispatch/test/extending_is_a.clj
@@ -3,20 +3,20 @@
         [dispatch.is-a-protocol :only [is-a? Is-A]])
   (:require [dispatch.multimethods :as mm]))
 
-(defn- map-is-a? [child parent h]
-  (and (map? parent)
+(defn- map-is-a? [parent child h]
+  (and (map? child)
        (let [sentinel (Object.)]
-         (every? 
-           (fn [[pk pv]] 
+         (every?
+           (fn [[pk pv]]
              (let [cv (get child pk sentinel)]
-               (if (identical? cv sentinel) 
+               (if (identical? cv sentinel)
                  false
-                 (is-a? h cv pv)))) 
+                 (is-a? h cv pv))))
            parent))))
 
 (extend-protocol Is-A
   clojure.lang.IPersistentMap
-  (-is-a? [c p h] (map-is-a? c p h)))
+  (-is-a? [p c h] (map-is-a? p c h)))
 
 (deftest maps-is-a?
   (testing "Maps:"
@@ -72,7 +72,7 @@
     (are [req resp] (= (handler req) resp)
          {:request-method :get}
          {:status 200, :headers {}, :body "Generic GET."}
-         
+
          {:request-method :post}
          {:status 404, :headers {}, :body "Not found."}
 
@@ -90,3 +90,64 @@
           :headers {"anything" "really", "app/client-status" "favored-nation"}
           :content-type "application/json"}
          {:status 200, :headers {}, :body "\"GET some super-special json.\""})))
+
+;; all right, let's get crazy.
+
+(defn- fn-is-a? [parent-fn child h] (boolean (parent-fn child)))
+
+(extend-protocol Is-A
+  clojure.lang.AFunction
+  (-is-a? [p c h]
+    (fn-is-a? p c h)))
+
+(defn- pattern-is-a? [parent-patt child h]
+  (and (string? child)
+       (boolean (re-matches parent-patt child))))
+
+(extend-protocol Is-A
+  java.util.regex.Pattern
+  (-is-a? [p c h]
+    (pattern-is-a? p c h)))
+
+(deftest function-is-a?
+  (testing "clojure.lang.AFunction's:"
+    (testing "identical"
+      (is (true? (is-a? number? number?)))
+      (is (false? (is-a? float? number?)))) ;; no logical implication, sorry :)
+    (testing "is the parent applied to the child truthy?"
+      (is (true? (is-a? 1 number?)))
+      (is (true? (is-a? 1.0 number?)))
+      (is (true? (is-a? 1M number?)))
+      (is (true? (is-a? "" string?)))
+      (is (true? (is-a? [1] seq)))
+
+      (is (false? (is-a? [] seq)))
+      (is (false? (is-a? () seq)))
+      (is (false? (is-a? {} seq)))
+      (is (false? (is-a? #{} seq))))
+    (testing "even anonymous fns work."
+      (is (true? (is-a? [:kw "str" 1] ;; first number is a 1?
+                        #(if-let [x (first (filter number? %))] (= x 1)))))
+      (is (false? (is-a? [:kw 2 "str" 1] ;; first number is a 1?
+                         #(if-let [x (first (filter number? %))] (= x 1))))))))
+
+(deftest regex-is-a?
+  (testing "java.util.regex.Pattern's:"
+    (testing "identical"
+      (is (false? (is-a? #"" #"")))        ; equality is not guaranteed; we
+      (is (false? (is-a? #"foo" #"foo")))  ; could fix the fn above to compare
+      (is (false? (is-a? #"bar" #"foo")))) ; them, if desired
+    (testing "does the parent 're-matches' the child?"
+      (is (true? (is-a? "" #"")))
+      (is (true? (is-a? "foo" #"foo")))
+      (is (true? (is-a? "food" #"foo.*")))
+      (is (false? (is-a? "x" #"")))
+      (is (false? (is-a? "food" #"foo")))
+      (is (false? (is-a? "for" #"foo.*"))))))
+
+(deftest crazy-is-a?
+  (testing "any crazy combination we want"
+    (is (true? (is-a? {:a 1       :b "foo"    :c Double :d "exact" :e :other}
+                      {:a number? :b #"foo.*" :c Number :d "exact"})))
+    (is (false? (is-a? {:a 1       :b "foo"    :c Double :d "exact" :e :other}
+                       {:a number? :b #"foo.+" :c Number :d "exact"})))))
